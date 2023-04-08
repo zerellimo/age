536c75bed17f88afb376a9ef317a12999537a573
zerellimo <12762091+zerellimo@users.noreply.github.com>
2023-04-08 16:58:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Day DD Month MM Year YYYY -- years -- months -- days</div>
+
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://zerellimo.github.io">Moez Ezzerelli</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
